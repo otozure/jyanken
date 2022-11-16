1226c44db31db8ab2744a564052d830a0b5b692c
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2022-11-16 11:56:25 +0900

    Update card.js

--- js/gamesystem/card.js
+++ js/gamesystem/card.js
@@ -1,4 +1,20 @@
+let gu_num;//グーの格納用の変数
+let ch_num;
+let pa_num;
 
 //ゲームスタート時にカードが配られる
+function getCards(){
+  
+}
+
 //じゃんけんすると使った手のカードがひとつ減る
+function lostACard(my_hand){
+  if(my_hand === "gu"){
+   gu_num = gu_num - 1;
+  }
+  if(my_hand === "ch"){
+   ch_num = ch_num - 1;
+  }
+}
+
 //※ 負けるとカードがもらえる
