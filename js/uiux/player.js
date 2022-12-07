1a6bc5b1fff7ac7ded2fc7f64497f20241fc6ddc
otozure <116129455+otozure@users.noreply.github.com>
2022-12-07 12:08:50 +0900

    Update player.js

--- js/uiux/player.js
+++ js/uiux/player.js
@@ -1,2 +1,10 @@
-スタートが押されたとき、ランダムでキャラクターを決める
-勝敗で表情を変える
+//スタートが押されたとき、ランダムでキャラクターを決める
+//勝敗で表情を変える
+function changeenemyimage(res){
+  if(res==="win"){
+    retun "enemy1A.jpg";
+  }
+else{
+  retun "enemy1B.jpg";
+}
+}
